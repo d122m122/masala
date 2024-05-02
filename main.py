d0e08e423476b3248b598aae72f729c15df481b5
Doniyor <domanick191201@gmail.com>
2024-05-02 18:28:37 +0500

    done masala

---
+++ main.py
@@ -0,0 +1,59 @@
+
+#TASK1
+# def array(num1, num2):
+#     arr = []
+#     for x in num2:
+#         if x > num1[0] and num1[-1] > x:
+#             arr.append(x)
+#     return arr
+#
+# print(array([1,9],[23,4,5,6,8]))
+# print(array([1,10],[5,8,43,56,11]))
+# print(array([7,32],[1,2,3,4,78]))
+
+
+#TASK2
+# def items(lst):
+#     nums = []
+#     for x in lst:
+#         all = x + 1
+#         nums.append(all)
+#     return nums
+#
+# print(items([0,1,2,3,4]))
+# print(items([2,4,6,8,10]))
+# print(items([-4,1,-2,3,8]))
+
+
+
+#TASK3
+# item = int(input("Youth: "))
+# def calc_age(years):
+#     year_days = 365
+#     total = item * year_days
+#     return f'{total} - Days'
+#
+# print(calc_age(item))
+
+
+#TASK4
+# while True:
+#     minum = int(input("MIN: "))
+#     maxim = int(input("MAX: "))
+#     def items(dict, lst):
+#         if maxim > minum:
+#             return True
+#         return False
+#
+#     print(items(minum, maxim))
+
+
+
+
+
+
+
+
+
+
+
